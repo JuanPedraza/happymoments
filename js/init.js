b817a1027aee21ad5bc0390fc165c56fbfaef038
Juan Pedraza <juan_pedraza@outlook.com>
2020-07-23 08:53:05 -0500

    Maquetacion de la web

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.fixed-action-btn').floatingActionButton();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
